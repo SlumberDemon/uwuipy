8e6909952c5b7e567a62d52ac081af11c11ce713
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-03-28 13:59:53 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="uwuipy",
+    version="0.0.3",
+    description="Uwuify text",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_dir={'': "src"},
+    url="https://github.com/SlumberDemon/uwuipy",
+    author="SlumberDemon",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ]
+)
